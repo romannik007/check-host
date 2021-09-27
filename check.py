c5751b5fb43e3b3b27fb4fb293203d19d1852fab
r.krilov <romannik007@mail.ru>
2021-09-27 14:20:31 +0300

    Update check.py and resources.conf

--- check.py
+++ check.py
@@ -3,8 +3,8 @@ import configparser
 cp = configparser.ConfigParser()
 check_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cp.read("./resources.conf")
-url = cp.get("resources", "url")
-port = cp.get("resources", "port")
+url = cp.get("resources", "check_url")
+port = cp.get("resources", "check_port")
 print(url, port)
 result_of_check = check_socket.connect_ex((url,int(port)))
 
@@ -14,9 +14,9 @@ if result_of_check == 0:
    print("Port is open")
 else:
    send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   send_url = cp.get("send", "url")
-   send_port = cp.get("send", "port")
-   send_line = '{"failed"' + ':' + '"' + url + ':' + port + '"' +'}'
+   send_url = cp.get("send", "send_url")
+   send_port = cp.get("send", "send_port")
+   send_line = '{"failed"' + ':' + '"' + check_url + ':' + check_port + '"' +'}'
    print(send_line)
    send_socket.connect((send_url,int(send_port)))
    send_socket.send(bytes(send_line, encoding = 'UTF-8'))
