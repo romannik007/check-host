f7845ab361728cd0b6d7279ea5c07be171fcabc2
r.krilov <romannik007@mail.ru>
2021-09-27 12:26:10 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -8,7 +8,7 @@ echo $send_url:$send_port
 server=ya.ru
 if nc -zv $check_url $check_port 2>/dev/null; then
     echo "$server ✓"
-    netcat $send_url $send_port <<< '{failed + : + " + $check_url + : + $check_port + " +}'
 else
     echo "$server ✗"
+    netcat $send_url $send_port <<< "{\"failed\" : \"$check_url:$check_port\"}"
 fi
